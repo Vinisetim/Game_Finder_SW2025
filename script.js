b245f2afcfb9a279ccd4133eb23fa6f7b6b8c094
Vinisetim <viniciusgueimis@gmail.com>
2025-10-13 08:56:06 -0300

    Add files via upload

--- script.js
+++
@@ -1,9 +0,0 @@
-function searchGame () {
-    const input = document.getElementById('Search');
-    const game = input.value.trim();
-    if (game === '') {
-        document.getElementById('mensagem').innerText = 'Please enter a game name.';
-    } else {
-        document.getElementById('mensagem').innerText = `Searching for ${game}...`;
-    };
-};
